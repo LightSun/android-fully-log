3a284e38352eba5a21c066d0e1aea75b93d7dfbb
LightSun <532278976@qq.com>
2016-07-07 11:57:53 +0800

    no

--- Android-fully-log/fully-log/src/main/java/com/heaven7/android/log/LogFilterOptions.java
+++ Android-fully-log/fully-log/src/main/java/com/heaven7/android/log/LogFilterOptions.java
@@ -8,53 +8,67 @@ import android.os.Parcelable;
      */
 public class LogFilterOptions implements Parcelable {
         public int level ;
-        public int lowestLevel ; //all >= level will allow
+        public int lowestLevel ;          //all >= level will allow
         public long startTime ;
         public long endTime ;
+
         public String tag ;
-        public String methodTag ;
-        public String exceptionName ;
+        public String tagPrefix ;
+        public String methodTag ;        //LiveController
+        public String methodTagPrefix ;
+
+        public String exceptionName ;     //java.lang.RuntimeException
+        public String exceptionShortName ;//RuntimeException
+
         public String dir ;
         public String content ;
 
-        @Override
-        public int describeContents() {
-            return 0;
+    public LogFilterOptions() {
         }
 
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeInt(this.level);
-            dest.writeInt(this.lowestLevel);
-            dest.writeLong(this.startTime);
-            dest.writeLong(this.endTime);
-            dest.writeString(this.tag);
-            dest.writeString(this.methodTag);
-            dest.writeString(this.exceptionName);
-            dest.writeString(this.dir);
-            dest.writeString(this.content);
-        }
-
-        public LogFilterOptions() {
-        }
-        private LogFilterOptions(Parcel in) {
-            this.level = in.readInt();
-            this.lowestLevel = in.readInt();
-            this.startTime = in.readLong();
-            this.endTime = in.readLong();
-            this.tag = in.readString();
-            this.methodTag = in.readString();
-            this.exceptionName = in.readString();
-            this.dir = in.readString();
-            this.content = in.readString();
-        }
-        public static final Parcelable.Creator<LogFilterOptions> CREATOR = new Parcelable.Creator<LogFilterOptions>() {
-            public LogFilterOptions createFromParcel(Parcel source) {
-                return new LogFilterOptions(source);
-            }
-
-            public LogFilterOptions[] newArray(int size) {
-                return new LogFilterOptions[size];
-            }
-        };
+    @Override
+    public int describeContents() {
+        return 0;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.level);
+        dest.writeInt(this.lowestLevel);
+        dest.writeLong(this.startTime);
+        dest.writeLong(this.endTime);
+        dest.writeString(this.tag);
+        dest.writeString(this.tagPrefix);
+        dest.writeString(this.methodTag);
+        dest.writeString(this.methodTagPrefix);
+        dest.writeString(this.exceptionName);
+        dest.writeString(this.exceptionShortName);
+        dest.writeString(this.dir);
+        dest.writeString(this.content);
+    }
+
+    private LogFilterOptions(Parcel in) {
+        this.level = in.readInt();
+        this.lowestLevel = in.readInt();
+        this.startTime = in.readLong();
+        this.endTime = in.readLong();
+        this.tag = in.readString();
+        this.tagPrefix = in.readString();
+        this.methodTag = in.readString();
+        this.methodTagPrefix = in.readString();
+        this.exceptionName = in.readString();
+        this.exceptionShortName = in.readString();
+        this.dir = in.readString();
+        this.content = in.readString();
+    }
+
+    public static final Creator<LogFilterOptions> CREATOR = new Creator<LogFilterOptions>() {
+        public LogFilterOptions createFromParcel(Parcel source) {
+            return new LogFilterOptions(source);
+        }
+
+        public LogFilterOptions[] newArray(int size) {
+            return new LogFilterOptions[size];
+        }
+    };
+}
